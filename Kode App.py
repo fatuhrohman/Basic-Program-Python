8e6b2c97f6fb2fc7001f5f19479ca5e24b0037fd
Muhammad Fatuhrohman <60308335+fatuhrohman@users.noreply.github.com>
2020-04-29 13:53:52 +0700

    Create Kode App.py

---
+++ Kode App.py
@@ -0,0 +1,39 @@
+# Program Management Kontak
+
+import function
+
+# list of dictionary
+daftar_kontak = []
+daftar_kontak.append({
+    "nama" : "Fatur",
+    "email" : "fatuhmfr11@gmail.com",
+    "telepon" : "085591903340"
+
+})
+# Menu Program
+while True:
+    print("# Menu")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Hapus Kontak")
+    print("4. Cari Kontak")
+    print("0. Keluar Program")
+
+    menu = input("Pilih menu : ")
+
+    if menu == "0":
+        break
+    elif menu == "1":
+        function.display_kontak(daftar_kontak)
+    elif menu == "2":
+        kontak = function.new_kontak()
+        daftar_kontak.append(kontak)
+    elif menu == "3":
+        function.hapus_kontak(daftar_kontak)
+    elif menu == "4":
+        function.cari_kontak(daftar_kontak)
+    else:
+        print("Menu tidak tersedia")
+        
+
+print("Program selesai berjalan, sampai jumpa")
